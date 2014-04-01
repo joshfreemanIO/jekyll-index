7f0794b3cf8dcfed88ab8f548ce5b3923f5c50c4
Josh Freeman <jdfreeman@satx.rr.com>
2014-04-01 14:40:10 -0500

    Documentation for _config.yaml; initial interator; copied Jekyll::Page initialize method

--- index_generator.rb
+++ index_generator.rb
@@ -3,15 +3,43 @@ module Jekyll
   #
   # # Index Generator
   #
+  # ```yaml
+  # indexgenerator:
+  # - item: tags
+  #   directory: blog/category
+  #   layout: blog-category-index
+
+  # - item: categories
+  #   directory: blog/tag
+  #   layout: blog-tag-index
+
+  # - item: authors
+  #   directory: blog/author
+  #   layout: blog-author-index
+  # ```
 
   class IndexGenerator < Generator
     safe true
 
+    def generate site
+      return unless site.config['indexgenerator']
 
+      site.config['indexgenerator'].each do |item|
+        puts item['index']
+      end
 
+    end
+  end
 
-    def initialize
-      @index_config = site.config['indexgenerator']
+  class IndexPage < Page
+    def initialize site, base, dir, name
+      @site = site
+      @base = base
+      @dir  = dir
+      @name = name
+
+      self.process(name)
+      self.read_yaml(File.join(base, dir), name)
     end
   end
 end
