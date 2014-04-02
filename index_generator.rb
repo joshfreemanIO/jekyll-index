70090ce8effa30253ded2c4a1310f893a1072a80
Josh Freeman <jdfreeman@satx.rr.com>
2014-04-01 23:43:02 -0500

    End of night. Need to figure out global scope

--- index_generator.rb
+++ index_generator.rb
@@ -1,4 +1,4 @@
-require 'pp'
+require 'yaml'
 module Jekyll
 
   #
@@ -6,17 +6,10 @@ module Jekyll
   #
   # ```yaml
   # indexgenerator:
-  # - item: tags
-  #   directory: blog/category
-  #   layout: blog-category-index
-
-  # - item: categories
-  #   directory: blog/tag
-  #   layout: blog-tag-index
-
-  # - item: authors
-  #   directory: blog/author
-  #   layout: blog-author-index
+  #   - name: Author
+  #     post_attribute: author_name
+  #     directory: blog/author
+  #     layout: blog-author-index
   # ```
 
   class IndexGenerator < Generator
@@ -26,16 +19,19 @@ module Jekyll
     def generate site
       return unless site.config['indexgenerator']
 
-      master_index = build_index site
-      master_index.each do |index|
-        index[:items].each do |index_name,index_items|
-          site.pages << IndexPage.new(site, index[:config], index_name, index_items)
+      master_index, indexes = build_index site
+      puts master_index
+      master_index.each do |name, hash|
+        hash[:items].each do |index_name,index_items|
+          site.pages << IndexPage.new(site, hash[:config], indexes, index_name, index_items)
         end
       end
+      File.open('/tmp/jekyll-index.yml', 'w') {|f| f.write master_index.to_yaml}
     end
 
     def build_index site
-      built_index = Array.new
+      built_index = Hash.new
+      indexes = Array.new
 
       site.config['indexgenerator'].each do |index|
         micro_index = {:config => index, :items => {} }
@@ -47,17 +43,23 @@ module Jekyll
           data.each do |index_name|
             micro_index[:items][index_name] ||= Array.new
             micro_index[:items][index_name] << post
+            indexes << index_name
           end
         end
-        built_index << micro_index
+        built_index[micro_index[:config]['name']] = micro_index
       end
 
-      return built_index
+      return built_index, indexes
+    end
+
+    def self.get_index name
+      master_index = YAML::load_file('/tmp/jekyll-index.yml')
+      master_index[name]
     end
   end
 
   class IndexPage < Page
-    def initialize site, index_config, index_name, index_items
+    def initialize site, index_config, indexes, index_name, index_items
 
       dir = File.join(index_config['directory'], index_name).downcase.gsub(/[^a-zA-Z0-9\/]/,'-')
 
@@ -66,14 +68,14 @@ module Jekyll
       @dir  = dir
       @name = 'index.html'
 
-      puts dir
-
       self.process(name)
       self.read_yaml File.join(@base, '_layouts'), index_config['layout'] + '.html'
 
       self.data['post_attribtute'] = index_config['post_attribtute']
       self.data['index_name'] = index_config['name']
-      self.data['index_items'] = index_items
+      self.data['indexes'] = indexes
+      self.data['name'] = index_name
+      self.data['items'] = index_items
     end
   end
 end
